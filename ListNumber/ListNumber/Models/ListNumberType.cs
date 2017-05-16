88ca8b4d722f34b01053cae5b51b61340004b7c5
tiangco <tiangco@technologist.com>
2017-05-16 17:23:42 -0400

    initial and WIP

---
+++ ListNumber/ListNumber/Models/ListNumberType.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListNumber.Models {
+	public struct ListNumberType {
+
+		private int _value;
+		public List<int> _listNumber;
+
+		/// <summary>
+		/// listString assumes the format 1->3->2->4, which represents a value of 4231"
+		/// </summary>
+		/// <param name="listString"></param>
+		public ListNumberType(string listString) {
+			try {
+				string[] numberArray = listString.Split(new string[] { "->" }, StringSplitOptions.None);
+				_listNumber = Array.ConvertAll(numberArray, Int32.Parse).ToList();
+
+				Array.Reverse(numberArray);
+				_value = int.Parse(string.Join(string.Empty, numberArray));
+			}
+			catch (Exception) {
+				throw;
+			}
+		}
+
+		public ListNumberType(int value) {
+			try {
+				_value = value;
+				char[] valueArray = _value.ToString().ToArray();
+				Array.Reverse(valueArray);
+				_listNumber = Array.ConvertAll(valueArray, c => (int)Char.GetNumericValue(c)).ToList();
+			}
+			catch (Exception) {
+				throw;
+			}
+		}
+
+		public ListNumberType(List<int> listNumber) {
+			try {
+				_listNumber = listNumber;
+
+				string[] numberArray = Array.ConvertAll(listNumber.ToArray(), c => c.ToString());
+				Array.Reverse(numberArray);
+				_value = int.Parse(string.Join(string.Empty, numberArray));
+
+			}
+			catch (Exception) {
+				throw;
+			}
+		}
+
+		public static ListNumberType operator +(ListNumberType ln1, ListNumberType ln2) {
+			return new ListNumberType(ln1._value + ln2._value);
+		}
+
+		public static ListNumberType operator *(ListNumberType ln1, ListNumberType ln2) {
+			return new ListNumberType(ln1._value * ln2._value);
+		}
+
+		public override string ToString() {
+			return string.Join("=>", _listNumber);
+		}
+
+	}
+
+}
---
+++ ListNumber/ListNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using ListNumber.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ListNumber {
+	class Program {
+		static void Main(string[] args) {
+
+			try {
+				ListNumberType ln = new ListNumberType("1>3>7");
+			}
+			catch (Exception ex) {
+				Console.WriteLine($"Error encountered: {ex.Message}");
+			}
+
+
+			ListNumberType ln2 = new ListNumberType(1234);
+
+			var x = new List<int>();
+			x.Add(7);
+			x.Add(8);
+			x.Add(9);
+			ListNumberType ln3 = new ListNumberType(x);
+
+			var addResult = Add(ln3, ln2);
+			Console.WriteLine(addResult);
+			Console.ReadLine();
+		}
+
+		private static ListNumberType Add(ListNumberType lnA, ListNumberType lnB) {
+			return lnA + lnB;
+		}
+
+		private static List<int> Add(List<int> list1, List<int> list2) {
+			ListNumberType ln1 = new ListNumberType(list1);
+			ListNumberType ln2 = new ListNumberType(list2);
+			return (ln1 + ln2)._listNumber;
+		}
+	}
+}
