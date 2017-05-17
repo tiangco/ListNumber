feefb6fb67cf6c619e00e4b1da960132b8e758ae
tiangco <tiangco@technologist.com>
2017-05-17 10:18:30 -0400

    final updates

--- ListNumber/ListNumber/Program.cs
+++ ListNumber/ListNumber/Program.cs
@@ -8,6 +8,8 @@ namespace ListNumber {
 
 			const string LISTOPERATOR_PARAMETER = "-operator:";
 			const string OPERANDS_PARAMETER = "-operands:";
+			const string ADD_OPERATOR = "add";
+			const string MULTIPLY_OPERATOR = "multiply";
 
 			bool isValid = true;
 			string listOperator = string.Empty;
@@ -20,16 +22,16 @@ namespace ListNumber {
 					switch (listOperator) {
 						case "+":
 						case "add":
-							listOperator = "add";
+							listOperator = ADD_OPERATOR;
 							break;
 						case "*":
 						case "mult":
 						case "multiply":
-							listOperator = "multiply";
+							listOperator = MULTIPLY_OPERATOR;
 							break;
 						default:
 							isValid = false;
-							Console.WriteLine($"{LISTOPERATOR_PARAMETER} parameter '{listOperator}' is invalid. 'add' and 'multiply' are the valid values.");
+							Console.WriteLine($"{LISTOPERATOR_PARAMETER} argument '{listOperator}' is invalid. '{ADD_OPERATOR}' and '{MULTIPLY_OPERATOR}' are the valid values.");
 							break;
 					}
 				}
@@ -39,7 +41,7 @@ namespace ListNumber {
 					string[] listArray = listOperands.Split(',');
 					if (listArray.Length != 2) {
 						isValid = false;
-						Console.WriteLine($"{OPERANDS_PARAMETER} parameter '{listOperands}' is invalid. Valid example: 1->2,3->4");
+						Console.WriteLine($"{OPERANDS_PARAMETER} argument '{listOperands}' is invalid. Valid example: '\"1->2,7->8\"'");
 					}
 					else {
 						GetOperand(out lnA, listArray[0], ref isValid);
@@ -50,7 +52,7 @@ namespace ListNumber {
 
 			if (isValid && (lnA._listNumber.Count == 0 || lnB._listNumber.Count == 0)) {
 				isValid = false;
-				Console.WriteLine("-operands: is required.");
+				Console.WriteLine($"{OPERANDS_PARAMETER} argument is required or invalid.");
 			}
 
 			if (!isValid) {
@@ -58,28 +60,31 @@ namespace ListNumber {
 				Console.WriteLine($" LineNumber {LISTOPERATOR_PARAMETER}(operator) {OPERANDS_PARAMETER}(operands)");
 				Console.WriteLine($"       operator  Valid values are 'add' or 'multiply'.");
 				Console.WriteLine($"       operands  Two lists separated by a comma. A list is of the format 'n[->n]...' where n is a digit from 0-9");
+				Console.WriteLine($"                 Note: Important Enclose the argument in quotes.");
 				Console.WriteLine();
-				Console.WriteLine($"Example: LineNumber {LISTOPERATOR_PARAMETER}add {OPERANDS_PARAMETER}1->2->3,8->7");
+				Console.WriteLine($"Example: LineNumber {LISTOPERATOR_PARAMETER}add {OPERANDS_PARAMETER}'1->2->3,8->7'");
 			}
 			else {
+				// Process that uses operator overrides
 				switch (listOperator) {
-					case "add":
+					case ADD_OPERATOR:
 						Console.WriteLine(lnA + lnB);
 						break;
-					case "multiply":
+					case MULTIPLY_OPERATOR:
 						Console.WriteLine(lnA * lnB);
 						break;
 					default:
 						break;
 				}
 
+				// Process that calls the requested methods Add() and Multiply()
 				switch (listOperator) {
-					case "add":
+					case ADD_OPERATOR:
 						List<int> result = Add(lnA._listNumber, lnB._listNumber);
 						ListNumberType ln = new ListNumberType(result);
 						Console.WriteLine(ln);
 						break;
-					case "multiply":
+					case MULTIPLY_OPERATOR:
 						result = Multiply(lnA._listNumber, lnB._listNumber);
 						ln = new ListNumberType(result);
 						Console.WriteLine(ln);
@@ -93,7 +98,6 @@ namespace ListNumber {
 			Console.ReadLine();
 		}
 
-		// GetOperand(out lnA, listArray[0], isValid)
 		private static void GetOperand(out ListNumberType ln, string representation, ref bool isValid) {
 			try {
 				ln = new ListNumberType(representation);
@@ -101,18 +105,9 @@ namespace ListNumber {
 			catch (Exception) {
 				isValid = false;
 				ln = new ListNumberType(string.Empty);
-				Console.WriteLine($"operand {representation} is invalid. Valid example 1->2->3");
+				Console.WriteLine($"argument {representation} is invalid. Valid example 1->2->3");
 			}
 		}
-
-		private static ListNumberType Add(ListNumberType lnA, ListNumberType lnB) {
-			return lnA + lnB;
-		}
-
-		private static ListNumberType Multiply(ListNumberType lnA, ListNumberType lnB) {
-			return lnA * lnB;
-		}
-
 
 		private static List<int> Add(List<int> list1, List<int> list2) {
 			ListNumberType ln1 = new ListNumberType(list1);
